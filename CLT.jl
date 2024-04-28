200c0ea3285a370c0072e6364e41ea92809a1ca4
Visruth <vsrimath@calpoly.edu>
2024-04-28 14:22:23 -0700

    Made some changes to structure, started basic report

--- CLT.jl
+++ CLT.jl
@@ -1,34 +1,16 @@
-using Distributions, Random, DataFrames, TidierPlots
-
-const sample_sizes = [5, 10, 20, 30, 40, 100, 200, 300, 400, 1000, 2000, 3000, 4000, 5000, 10000]
-const distributions = [
-    (LogNormal, [0, 1.4865], 31.65266),
-    (Poisson, [0.001], 31.6228),
-    (Gamma, [0.02], 20),
-    (LogNormal, [0, 1], 6.1849),
-    (Exponential, [], 2),
-    (LogNormal, [0, 0.5], 1.7502),
-    (LogNormal, [0, 0.25], 0.7883),
-    (Beta, [0.3, 0.2], -0.4),
-    (Normal, [], 0)
-]
+using Distributions, Random, DataFrames, CSV
 
 function analysis(statistic, distro, n, r, params...)
     Random.seed!(0)
     d = distro(params...)
 
     sample_statistics = zeros(r)
+    Threads.@threads for i in 1:r
+        sample_statistics[i] = statistic(rand(d, n))
+    end
 
-    # sample_statistics = pmap(x -> statistic(rand(d, n)), 1:r)
-
-    sample_statistics = [statistic(rand(d, n)) for i in 1:r]
-
-    # Threads.@threads for i in 1:r
-    #     sample_statistics[i] = statistic(rand(d, n))
-    # end
-
-    s = std(sample_statistics)
     m = mean(sample_statistics)
+    s = std(sample_statistics)
 
     upper = sum(sample_statistics .>= m + 1.96 * s) / r
     lower = sum(sample_statistics .<= m - 1.96 * s) / r
@@ -37,6 +19,20 @@ function analysis(statistic, distro, n, r, params...)
 end
 
 function analyze_distributions(r)
+    println("Analyzing distributions with $(r) repetitions")
+
+    sample_sizes = [5, 10, 20, 30, 40, 100, 200, 300, 400, 1000, 2000, 3000, 4000, 5000, 10000]
+    distributions = [
+        (LogNormal, [0, 1.4865], 31.65266),
+        (Poisson, [0.001], 31.6228),
+        (Gamma, [0.02], 20),
+        (LogNormal, [0, 1], 6.1849),
+        (Exponential, [], 2),
+        (LogNormal, [0, 0.5], 1.7502),
+        (LogNormal, [0, 0.25], 0.7883),
+        (Beta, [0.3, 0.2], -0.4),
+        (Normal, [], 0)
+    ]
     results = DataFrame(Distribution=String[], Params=Vector[], Skewness=Float64[], Sample_Size=Int[], Upper_Tail=Float64[], Lower_Tail=Float64[], Tail_Sum=Float64[], Tail_Difference=Float64[])
 
     for (distro, params, skewness) in distributions
@@ -50,30 +46,8 @@ function analyze_distributions(r)
     results
 end
 
-analyze_distributions(1)
-@profview analyze_distributions(10000)
-
-results = DataFrame(Distribution=String[], Params=Vector[], Skewness=Float64[], Sample_Size=Int[], Upper_Tail=Float64[], Lower_Tail=Float64[], Tail_Sum=Float64[], Tail_Difference=Float64[])
-
-@time begin
-    for (distro, params, skewness) in distributions
-        println("$(distro) with parameters $(params)")
-        for n in sample_sizes
-            upper, lower, tail_sum, tail_diff = analysis(mean, distro, n, params...)
-            push!(results, (string(distro), params, skewness, n, upper, lower, tail_sum, tail_diff))
-        end
-    end
-end
-
+analyze_distributions(1) # compile
+results = analyze_distributions(10000)
 display(results)
 
-filtered = filter(row -> row[:Params] in [[]], results)
-
-ggplot(filtered, aes(x=:Sample_Size, y=:Tail_Sum, color=:Distribution)) +
-geom_line() +
-geom_point() +
-theme_minimal() +
-labs(title="Central Limit Theorem", x="Sample Size", y="Tail Sum")
-
-# TODO Format results in table
-# TODO Plot results, lin reg?
+CSV.write("CLT.csv", results)
