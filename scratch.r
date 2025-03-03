d07acc4f9eaeadfbc1a0b7a3163bd71307f69e2a
VisruthSK <67435125+VisruthSK@users.noreply.github.com>
2025-03-02 20:11:03 -0800

    Trying some things with exp pop

--- scratch.r
+++ scratch.r
@@ -1,16 +1,4 @@
 library(tidyverse)
-# distributions = [
-#   Gamma(16),
-#   LogNormal(0, 0.25),
-#   Gamma(4),
-#   Gamma(2),
-#   LogNormal(0, 0.5),
-#   Gamma(1),
-#   Exponential(),
-#   Gamma(0.64),
-#   LogNormal(0, 0.75)
-# ]
-
 set.seed(0)
 
 # population skewness as response
@@ -24,29 +12,44 @@ sampling_distribution <- \(distro, r)
   replicate(r, adjusted_skewness(eval(distro)))
 
 generate_data <- function(n, r, population_skews) {
-  raw_distributions <- list(
+  sampling_distributions <- list(
     sampling_distribution(expr(rgamma(!!n, 16)), r),
     sampling_distribution(expr(rlnorm(!!n, 0, 0.25)), r),
     sampling_distribution(expr(rgamma(!!n, 4)), r),
     sampling_distribution(expr(rgamma(!!n, 2)), r),
     sampling_distribution(expr(rlnorm(!!n, 0, 0.5)), r),
     sampling_distribution(expr(rgamma(!!n, 1)), r),
-    sampling_distribution(expr(rf(!!n, 10, 15)), r),
     sampling_distribution(expr(rgamma(!!n, 0.64)), r),
     sampling_distribution(expr(rlnorm(!!n, 0, 0.75)), r)
   )
-  mss <- map_dbl(raw_distributions, mean)
+  distribution_names <- c(
+    "Gamma (α = 16, θ = 1)",
+    "Log-normal (μ = 0, σ = 0.25)",
+    "Gamma (α = 4, θ = 1)",
+    "Gamma (α = 2, θ = 1)",
+    "Log-normal (μ = 0, σ = 0.5)",
+    "Gamma (α = 1, θ = 1)",
+    "Gamma (α = 0.64, θ = 1)",
+    "Log-normal (μ = 0, σ = 0.75)"
+  )
 
   data.frame(
+    distribution = distribution_names,
     pop_skewness = population_skews,
     sample_size = n,
-    mean_sampling_skewness = mss
+    mean_sampling_skewness = map_dbl(sampling_distributions, mean),
+    median_sampling_skewness = map_dbl(sampling_distributions, median),
+    lower_quantile = map_dbl(
+      sampling_distributions,
+      \(x) quantile(abs(x), 0.025)
+    ),
+    upper_quantile = map_dbl(
+      sampling_distributions,
+      \(x) quantile(abs(x), 0.975)
+    )
   )
 }
 
-skew_f <- \(d1, d2)
-  ((2 * d1 + d2 - 2) * sqrt(8 * (d2 - 4))) /
-    ((d2 - 6) * sqrt(d1 * (d1 + d2 - 2)))
 skew_lnorm <- \(sigma) (exp(sigma^2) + 2) * sqrt(exp(sigma^2) - 1)
 population_skews <- c(
   0.5,
@@ -55,15 +58,17 @@ population_skews <- c(
   sqrt(2),
   skew_lnorm(0.5),
   2,
-  skew_f(10, 15),
   2.5,
   skew_lnorm(0.75)
 )
 ns <- c(seq(10, 50, 10), seq(50, 200, 25)) |> unique()
 r <- 1e5
 
-# skew_data <- map_df(ns, \(n) generate_data(n, r, population_skews)) |> write_csv(here::here("skew_data.csv"))
+skew_data <- map_df(ns, \(n) generate_data(n, r, population_skews)) |>
+  write_csv(here::here("skew_data.csv"))
 skew_data <- read_csv(here::here("skew_data.csv"))
+# skew_data |> mutate(Median = (lower_quantile + upper_quantile) / 2) |> View()
+
 skew_data |>
   ggplot(
     aes(
@@ -82,29 +87,113 @@ skew_data |>
     y = "Mean Sampling Skewness",
     color = "Sample Size"
   ) +
-  theme_minimal()
+  theme_bw()
 
 skew_data |>
-  mutate(percent = mean_sampling_skewness / pop_skewness) |>
+  mutate(
+    percent = mean_sampling_skewness / pop_skewness,
+    distribution = fct(
+      distribution,
+      levels = unique(distribution[order(pop_skewness)])
+    )
+  ) |>
   ggplot(
-    aes(x = sample_size, y = percent, color = fct(as.character(pop_skewness)))
+    aes(x = sample_size, y = percent, color = distribution)
   ) +
   geom_point() +
   geom_line() +
   geom_hline(yintercept = 1) +
-  # geom_smooth(method = "lm", se = FALSE) +
-  theme_minimal() +
-  geom_text(
+  theme_bw() +
+  ggrepel::geom_label_repel(
     data = skew_data |>
       mutate(percent = mean_sampling_skewness / pop_skewness) |>
       group_by(pop_skewness) |>
       slice_tail(n = 1),
     aes(label = round(percent, 2), x = sample_size + 0.5),
     show.legend = FALSE,
-    hjust = 0,
-    size = 3,
-    fontface = "bold"
+    size = 3.5,
+    fontface = "bold",
+    segment.color = "grey",
+    min.segment.length = 0,
+    force = 2,
+    nudge_x = 10,
+    direction = "y",
+    max.overlaps = 17
+  ) +
+  labs(
+    title = "Convergence Rate of Sample Skewness to Population Skewness",
+    x = "Sample Size",
+    y = "Percent of Population Skewness",
+    color = "Distribution"
   )
+
+ggsave(
+  here::here("Figures", "skewness_convergence.png"),
+  width = 12,
+  height = 6.75,
+  dpi = 1000
+)
+
+exp_data <- skew_data |>
+  filter(distribution == "Gamma (α = 1, θ = 1)") |>
+  mutate(
+    log_correction = log(pop_skewness / mean_sampling_skewness - 1),
+    log_sample_size = log(sample_size)
+  )
+exp_data |>
+  ggplot(aes(x = log_sample_size, y = log_correction)) +
+  geom_point() +
+  theme_bw() +
+  geom_smooth(se = FALSE) +
+  labs(
+    x = "Log Sample Size",
+    y = "Log Correction Factor", # (log(pop_skewness/mean_sampling_skewness - 1)
+    title = "Linear Relationship Between Log Sample Size and Log Correction Factor"
+  )
+
+model <- lm(log_correction ~ log_sample_size, data = exp_data)
+summary(model)
+
+coeffs <- coef(model)
+exp_corrected_skewness <- function(sample_skew, n) {
+  unname(sample_skew * (exp(coeffs[1] + coeffs[2] * log(n)) + 1))
+}
+
+skew_data |>
+  filter(pop_skewness < 3) |>
+  mutate(
+    corrected_skew = exp_corrected_skewness(
+      mean_sampling_skewness,
+      sample_size
+    ),
+    percent = corrected_skew / pop_skewness,
+    distribution = fct(
+      distribution,
+      levels = unique(distribution[order(pop_skewness)])
+    )
+  ) |>
+  ggplot(aes(x = sample_size, y = percent, color = distribution)) +
+  geom_point() +
+  geom_hline(yintercept = 1)
+
+# REMOVE FROM HERE ON
+perskewgamma <- lapply(
+  ns,
+  \(n) sampling_distribution(expr(rgamma(!!n, 4)), r)
+) |>
+  map_dbl(\(x) ecdf(x)(1))
+
+plot(ns, perskewgamma)
+
+perskewlnorm <- lapply(
+  ns,
+  \(n) sampling_distribution(expr(rlnorm(!!n, 0, 0.5)), r)
+) |>
+  map_dbl(\(x) ecdf(x)(1.75))
+
+plot(ns, perskewlnorm)
+
+modele <- lm(c(perskewgamma, perskewlnorm) ~ rep(ns, 2))
 
 # TODO: regress pop skewness on mss and n
 # TODO: look at each predictor separately
