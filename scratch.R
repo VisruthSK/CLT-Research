e444a9368d673974e36bc90ddc061286fc702368
VisruthSK <public@visruth.com>
2026-04-08 05:32:37 -0700

    t fits/corrections

--- scratch.R
+++ scratch.R
@@ -1,6 +1,7 @@
 library(tidyverse)
 library(flextable)
 set.seed(0)
+
 t_stats <- read_csv("t_statistics.csv.gz", show_col_types = FALSE) |>
   mutate(Total = `Upper Tail` + `Lower Tail`)
 
@@ -194,14 +195,18 @@ ggsave(
   dpi = plot_dpi
 )
 
-model <- lm(Skewness ~ sqrt(`Sample Size`), table_df)
-summary(model)
+pop_model <- lm(Skewness ~ sqrt(`Sample Size`), table_df)
+summary(pop_model)
 
-skew_plot <- table_df |>
-  ggplot(aes(x = sqrt(`Sample Size`), y = Skewness, label = Distribution)) +
+pop_skew_plot <- table_df |>
+  ggplot(aes(
+    x = sqrt(`Sample Size`),
+    y = Skewness,
+    label = Distribution
+  )) +
   geom_abline(
-    slope = coef(model)[2],
-    intercept = coef(model)[1],
+    slope = coef(pop_model)[2],
+    intercept = coef(pop_model)[1],
     color = "blue",
     linetype = "dashed",
     linewidth = 1.5
@@ -209,9 +214,12 @@ skew_plot <- table_df |>
   geom_line(linewidth = 1.5) +
   geom_point(size = 3) +
   labs(
-    title = "Linear Relationship between Skewness and Empirical Minimum Square Root Sample Size for t Statistics",
+    title = paste(
+      "Linear Relationship between Population Skewness and",
+      "Empirical Minimum Square Root Sample Size for t Statistics"
+    ),
     x = "Square Root of Sample Size",
-    y = "Skewness"
+    y = "Population Skewness"
   ) +
   theme_bw() +
   theme(
@@ -222,9 +230,158 @@ skew_plot <- table_df |>
   ggrepel::geom_label_repel(seed = 0, nudge_x = 1, size = 5)
 
 ggsave(
-  here::here("Figures", paste0("Skew_Sample_Size_t_statistics_", on, ".png")),
-  plot = skew_plot,
+  here::here(
+    "Figures",
+    paste0("Skew_Sample_Size_t_statistics_pop_", on, ".png")
+  ),
+  plot = pop_skew_plot,
   width = 15,
   height = 8.5,
   dpi = plot_dpi
+)
+
+exp_data <- t_stats |>
+  filter(str_detect(Distribution, "^Exponential")) |>
+  mutate(
+    log_correction = log((Skewness / `Average Sample Skewness`) - 1),
+    log_sample_size = log(`Sample Size`)
+  )
+
+model <- lm(log_correction ~ log_sample_size, data = exp_data)
+summary(model)
+
+coeffs <- coef(model)
+exp_corrected_skewness <- \(skew, n) {
+  unname(skew * (exp(coeffs[1] + coeffs[2] * log(n)) + 1))
+}
+
+corrected_table_df <- table_df |>
+  left_join(
+    t_stats |>
+      mutate(Distribution = str_replace(Distribution, "\\{.*\\}", " ")) |>
+      select(
+        Distribution,
+        `Sample Size`,
+        `Average Sample Skewness`
+      ),
+    by = c("Distribution", "Sample Size")
+  ) |>
+  mutate(
+    `Corrected Skewness` = exp_corrected_skewness(
+      `Average Sample Skewness`,
+      `Sample Size`
+    )
+  )
+
+sample_skewness_breaks <- c(0.25, 0.5, 0.75, 1.0, 1.5, 2.0)
+pop_model_coeffs <- coef(pop_model)
+
+sample_skewness_lookup <- map_int(
+  sample_skewness_breaks,
+  \(sample_skewness) {
+    n_grid <- 5:10000
+    n_grid[
+      which(
+        (pop_model_coeffs[1] +
+          pop_model_coeffs[2] * sqrt(n_grid)) /
+          exp_corrected_skewness(1, n_grid) >=
+          sample_skewness
+      )[1]
+    ]
+  }
+)
+
+tibble(
+  Metric = c("Sample skewness", "Min sample size"),
+  `.25` = c(".25", as.character(sample_skewness_lookup[1])),
+  `.5` = c(".5", as.character(sample_skewness_lookup[2])),
+  `.75` = c(".75", as.character(sample_skewness_lookup[3])),
+  `1.0` = c("1.0", as.character(sample_skewness_lookup[4])),
+  `1.5` = c("1.5", as.character(sample_skewness_lookup[5])),
+  `2.0` = c("2.0", as.character(sample_skewness_lookup[6]))
+) |>
+  print() |>
+  flextable() |>
+  font(fontname = "Trebuchet MS") |>
+  autofit() |>
+  save_as_image(
+    path = here::here(
+      "Figures",
+      paste0("table_t_statistics_sample_skewness_", on, ".png")
+    ),
+    res = table_res
+  )
+
+# Ensuring table/correction matches means
+means_sample_skewness_breaks <- c(0.25, 0.5, 0.75, 1.0, 1.5, 2.0)
+slide_table <- c(10, 20, 35, 50, 100, 165)
+
+means_table_df <- read_csv("means.csv", show_col_types = FALSE) |>
+  filter(
+    `Lower Tail` >= 0.02 & `Lower Tail` <= 0.03,
+    `Upper Tail` >= 0.02 & `Upper Tail` <= 0.03
+  ) |>
+  group_by(Distribution) |>
+  slice_min(`Sample Size`, n = 1, with_ties = TRUE) |>
+  ungroup() |>
+  select(Distribution, Skewness, `Sampling Skewness`, `Sample Size`) |>
+  arrange(Skewness, Distribution)
+
+means_pop_model <- lm(Skewness ~ sqrt(`Sample Size`), means_table_df)
+
+skew_data <- read_csv("skew_data.csv", show_col_types = FALSE)
+
+means_exp_data <- skew_data |>
+  filter(distribution == "Gamma (α = 1, θ = 1)") |>
+  mutate(
+    log_correction = log((pop_skewness / mean_sampling_skewness) - 1),
+    log_sample_size = log(sample_size)
+  )
+
+means_correction_model <- lm(
+  log_correction ~ log_sample_size,
+  data = means_exp_data
+)
+means_coeffs <- coef(means_correction_model)
+
+means_exp_corrected_skewness <- \(skew, n) {
+  unname(skew * (exp(means_coeffs[1] + means_coeffs[2] * log(n)) + 1))
+}
+
+means_pop_model_coeffs <- coef(means_pop_model)
+
+means_sample_skewness_lookup <- map_int(
+  means_sample_skewness_breaks,
+  \(sample_skewness) {
+    n_grid <- 5:10000
+    n_grid[
+      which(
+        (means_pop_model_coeffs[1] +
+          means_pop_model_coeffs[2] * sqrt(n_grid)) /
+          means_exp_corrected_skewness(1, n_grid) >=
+          sample_skewness
+      )[1]
+    ]
+  }
+)
+
+means_slide_formula_lookup <- map_int(
+  means_sample_skewness_breaks,
+  \(sample_skewness) {
+    n_grid <- 5:10000
+    n_grid[
+      which(
+        n_grid^1.365 /
+          (6 * (n_grid^0.865 + exp(1.696))) >=
+          sample_skewness
+      )[1]
+    ]
+  }
+)
+
+tibble(
+  sample_skewness = means_sample_skewness_breaks,
+  clt_r_lookup = means_sample_skewness_lookup,
+  slide_formula_lookup = means_slide_formula_lookup,
+  slide_table = slide_table
 )
